87d48168b291bbe4c7bfb7907f73f7e0e99eb9ea
xTbeniaminTx <beniamin.tolan@outlook.com>
2021-05-26 11:01:21 +0200

    initial files with SpringInit

---
+++ src/main/java/fr/tolan/safetynetalerts/SafetyNetAlertsApplication.java
@@ -0,0 +1,13 @@
+package fr.tolan.safetynetalerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetAlertsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SafetyNetAlertsApplication.class, args);
+  }
+
+}
